255c1484627b6bad7f3c265ccc82d66f52bafa8b
Akhira Nandan <akhiranandanp89548@gmail.com>
2023-11-26 14:34:28 +0530

    adjusting size of display cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,7 +7,7 @@ const Card = (props) => {
           className="card mt-3"
           style={{ width: "18rem", maxHeight: "360px" }}
         >
-          <img src={props.imgSrc} className="card-img-top" alt="..." />
+          <img src={props.imgSrc} className="card-img-top" alt="..." style={{height:"120px", objectFit:"fill"}}/>
           <div className="card-body">
             <h5 className="card-title">{props.foodName}</h5>
             <div className="container w-100">
